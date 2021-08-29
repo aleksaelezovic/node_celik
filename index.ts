4f8b44408a737af28c075bdb4a8e5b8e92dd2292
BuildTools <unconfigured@null.spigotmc.org>
2021-08-29 23:10:18 +0200

    Updated main (index.ts). Added main class MUPCelikApi, uses promises

--- index.ts
+++ index.ts
@@ -1,106 +1,131 @@
-console.log("---running!---");
 import CelikApi, { CelikApiResponseStatus } from "./models/api/api";
 import { CardType, CertificateType, SignatureID } from "./models/api/types";
 
-const smartcard = require("smartcard");
-const Devices = smartcard.Devices;
-const devices = new Devices();
+export default class MUPCelikApi {
+  private api: CelikApi;
+  device: string = "";
+  constructor(
+    device = "",
+    cardType: CardType = CardType.DEFAULT,
+    private isInit = false
+  ) {
+    this.api = new CelikApi();
+    this.start();
+    if (device) this.startReadSync(device, cardType);
+  }
 
-devices.onActivated().then((e: any) => {
-  const DEVICE = e.device.name;
-  const CARD = e.device.card;
-  e.device.on("card-inserted", (e: any) => {
-    console.log(e.card.getAtr());
+  private start() {
+    const status = this.api.startup().status;
+    if (status !== CelikApiResponseStatus.OK) throw new Error(status);
+    this.isInit = true;
+  }
+  private startReadSync(device: string, cardType: CardType = CardType.DEFAULT) {
+    if (!device) return;
+    const status = this.api.beginRead(device, cardType).status;
+    if (status !== CelikApiResponseStatus.OK) throw new Error(status);
+    this.device = device;
+  }
 
-    const api = new CelikApi();
-    console.log(api.cleanup().status);
-    let status = api.startup().status;
-    if (status === CelikApiResponseStatus.OK) {
-      console.log("STARTED!");
+  async startRead(device: string, cardType: CardType = CardType.DEFAULT) {
+    return this.startReadSync(device, cardType);
+  }
 
-      status = api.beginRead(DEVICE, CardType.CARD_ID2020).status;
-      if (status === CelikApiResponseStatus.OK) {
-        console.log("READ - START!");
+  async readAllData() {
+    const res = await Promise.all([
+      this.readDocumentData(),
+      this.readFixedPersonalData(),
+      this.readVariablePersonalData(),
+      this.readPortrait(),
+    ]);
+    return {
+      ...res[0],
+      ...res[1],
+      ...res[2],
+      portrait: res[3],
+    };
+  }
+  async readDocumentData() {
+    const res = this.api.readDocumentData();
+    if (res.status !== CelikApiResponseStatus.OK) throw new Error(res.status);
+    return res.data!;
+  }
+  async readFixedPersonalData() {
+    const res = this.api.readFixedPersonalData();
+    if (res.status !== CelikApiResponseStatus.OK) throw new Error(res.status);
+    return res.data!;
+  }
+  async readVariablePersonalData() {
+    const res = this.api.readVariablePersonalData();
+    if (res.status !== CelikApiResponseStatus.OK) throw new Error(res.status);
+    return res.data!;
+  }
+  async readPortrait(): Promise<Buffer> {
+    const res = this.api.readPortrait();
+    if (res.status !== CelikApiResponseStatus.OK) throw new Error(res.status);
+    return res.data!.portrait;
+  }
+  async readCertificate(certType: CertificateType): Promise<Buffer> {
+    const res = this.api.readCertificate(certType);
+    if (res.status !== CelikApiResponseStatus.OK) throw new Error(res.status);
+    return res.data!.certificate;
+  }
+  async verifyAllSignatures() {
+    await this.verifySignature(SignatureID.CARD);
+    await this.verifySignature(SignatureID.FIXED);
+    await this.verifySignature(SignatureID.VARIABLE);
+    console.log("ALL VERIFIED!");
+  }
+  async verifySignature(sigId: SignatureID) {
+    const status = this.api.verifySignature(sigId).status;
+    if (status !== CelikApiResponseStatus.OK) throw new Error(status);
+  }
 
-        status = api.verifySignature(SignatureID.CARD).status;
-        if (status !== CelikApiResponseStatus.OK) {
-          console.log("ERROR in VERIFY, card!");
-          return process.exit();
-        }
-        status = api.verifySignature(SignatureID.FIXED).status;
-        if (status !== CelikApiResponseStatus.OK) {
-          console.log("ERROR in VERIFY, fixed!");
-          return process.exit();
-        }
-        status = api.verifySignature(SignatureID.VARIABLE).status;
-        if (status !== CelikApiResponseStatus.OK) {
-          console.log("ERROR in VERIFY, variable!");
-          return process.exit();
-        }
-        console.log("VERIFIED!");
+  async endRead() {
+    if (!this.device) return;
+    const status = this.api.endRead().status;
+    if (status !== CelikApiResponseStatus.OK) throw new Error(status);
+    this.device = "";
+  }
+  async end() {
+    await this.endRead();
+    if (!this.isInit) return;
+    const status = this.api.cleanup().status;
+    if (status !== CelikApiResponseStatus.OK) throw new Error(status);
+    this.isInit = false;
+  }
+}
+export { MUPCelikApi };
 
-        /* -------TEST--------- */
-        let res: any = api.readDocumentData();
-        if (res.status === CelikApiResponseStatus.OK) {
-          console.log("DATA:");
-          const data = res.data!;
-          console.log(data);
-        } else {
-          console.log(res.status);
-        }
-        /* -------TEST FIXED--- */
-        res = api.readFixedPersonalData();
-        if (res.status === CelikApiResponseStatus.OK) {
-          console.log("DATA:");
-          const data = res.data!;
-          console.log(data);
-        } else {
-          console.log(res.status);
-        }
-        /* -------TEST VAR----- */
-        res = api.readVariablePersonalData();
-        if (res.status === CelikApiResponseStatus.OK) {
-          console.log("DATA:");
-          const data = res.data!;
-          console.log(data);
-        } else {
-          console.log(res.status);
-        }
-        /* -------TEST IMG----- */
-        res = api.readPortrait();
-        if (res.status === CelikApiResponseStatus.OK) {
-          console.log("DATA:");
-          const data = res.data as any;
-          console.log(data["portrait"].toString("base64"));
-        } else {
-          console.log(res.status);
-        }
-        /* -------TEST CERT---- */
-        const c = api.readCertificate(CertificateType.CERT_USER_1);
-        if (c.status === CelikApiResponseStatus.OK) {
-          console.log("DATA:");
-          const data = c.data as any;
-          console.log(data["certificate"].toString("base64"));
-        } else {
-          console.log(c.status);
-        }
-
-        status = api.endRead().status;
-        if (status === CelikApiResponseStatus.OK) {
-          console.log("READ - END!");
-        }
-      } else {
-        console.log(status);
-      }
-
-      status = api.cleanup().status;
-      if (status === CelikApiResponseStatus.OK) {
-        console.log("CLEANUP!");
-        console.log("---end!---");
-      }
-    } else {
-      console.log(status);
-    }
+const _test = async (device: string) => {
+  console.log("---START---");
+  const celik = new MUPCelikApi(device);
+  try {
+    await celik.verifyAllSignatures();
+    const data = await celik.readAllData();
+    console.log(data);
+    // const cert = await celik.readCertificate(CertificateType.CERT_USER_1); // doesnt work, unable to execute
+    // console.log(cert);
+  } catch (e: any) {
+    console.log(e.message ?? new Error(e));
+  } finally {
+    await celik.end();
+    console.log("---END---");
     process.exit();
+  }
+};
+
+/* EXAMPLE */
+const smartcard = require("smartcard");
+const devices = new smartcard.Devices();
+
+devices.on("device-activated", (e: any) => {
+  console.log("---Device:", e.device.name);
+  console.log("Waiting for card...");
+
+  e.device.on("card-inserted", (e: any) => {
+    console.log("---Card:", e.card.getAtr());
+
+    _test(e.device.name);
   });
 });
+console.log("Waiting for device...");
